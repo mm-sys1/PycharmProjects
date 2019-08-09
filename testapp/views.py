7e0213bef7bc576c69f3041c69f8776ccea0313c
mm-sys1 <hozan@test.de>
2019-08-09 17:36:37 +0200

    View und Template für Blogpost-Details sowie CSS für die Website hinzugefügt

--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 # Create your views here.
@@ -10,3 +11,7 @@ def post_list (request):
 
 def create_post(request):
     pass
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'testapp/post_detail.html', {'post': post})
