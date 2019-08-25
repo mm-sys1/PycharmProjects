042426b02146ccd468b789824e30e02c89c6ba8a
mm-sys1 <hozan.hasan@stud.hs-hannover.de>
2019-08-26 00:37:33 +0200

    first commit

--- testapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('list/', views.post_list, name='post_list'),
-    path('test/', views.create_post, name='create_post'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-]
--- testapp/views.py
+++ testapp/views.py
@@ -1,17 +1,3 @@
 from django.shortcuts import render
-from .models import Post
-from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
 
 # Create your views here.
-def post_list (request):
-    posts = Post.objects.all()
-    return render(request, 'testapp/post_list.html', {'bala': posts})
-
-
-def create_post(request):
-    pass
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'testapp/post_detail.html', {'post': post})
